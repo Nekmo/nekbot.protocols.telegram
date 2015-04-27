2fe0d56c0260186e508e242c44727ea5bed1ee92
Nekmo <contacto@nekmo.com>
2015-04-27 02:31:50 +0200

    Petición de permisos funcionando en Telegram.

--- nekbot/protocols/telegram/user.py
+++ nekbot/protocols/telegram/user.py
@@ -5,4 +5,10 @@ from nekbot.protocols import User
 
 class UserTelegram(User):
     def __init__(self, protocol, user):
+        self.user = user
         User.__init__(self, protocol, user.name, user.id)
+
+    def send_message(self, body, notice=False):
+        if not isinstance(body, (str, unicode)):
+            body = str(body)
+        self.protocol.tg.msg('user#%s' % self.id, body)
