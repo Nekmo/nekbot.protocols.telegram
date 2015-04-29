1ab078fb69c14e69c30b67c9cf575ea243d6367d
Nekmo <contacto@nekmo.com>
2015-04-29 04:39:03 +0200

    [default] [Test] Añadido VERSION.

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ from pip.req import parse_requirements
 import uuid
 
 
+VERSION = open('VERSION').read().replace('\n', '')
+ROOT_INCLUDE = ['requirements.txt', 'VERSION', 'LICENSE.txt']
+
+
 requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
 install_requires = [str(ir.req) for ir in requirements if not ir.url]
 
 setup(
     name='nekbot.protocols.telegram',
     namespace_packages = ['nekbot.protocols'],
-    version='0.1',
+    version=VERSION,
 
     description='Telegram Protocol for Nekbot, a modular multiprotocol bot.',
 
@@ -65,6 +69,8 @@ setup(
             'telegram = nekbot.protocols.telegram:Telegram',
         ],
     },
+        
+    package_data = {'': ROOT_INCLUDE},
 
     zip_safe=False,
 )
